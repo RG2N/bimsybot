42381d4f11981a3b4d71b6a7ab4d800c5b53c286
RG2N <64289295+RG2N@users.noreply.github.com>
2023-07-02 12:58:11 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const fs = require('node:fs');
-const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, EmbedBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder, ActionRowBuilder } = require('discord.js');
 const { token } = require('./config.json');
+const Gamedig = require('gamedig');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
@@ -11,6 +10,33 @@ client.commands = new Collection();
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+const activities = [
+    "Hypnotic",
+    "with Ruby"
+];
+
+client.on("ready", () => {
+    // run every 10 seconds
+    setInterval(() => {
+      // generate random number between 0 and length of array.
+      const randomIndex = Math.floor(Math.random() * activities.length);
+      const newActivity = activities[randomIndex];
+      Gamedig.query({
+        type: 'unturned',
+        host: '104.129.132.73',
+        port: '27095'
+        }).then((state) => {
+            //console.log(state);
+            let activ2 = `${newActivity} (${state.players.length}/${state.maxplayers})`
+            client.user.setActivity(activ2);
+        })//.catch((error) => {
+        //     console.log("Server is offline");
+        //     let activ3 = `${newActivity} (Offline/Offline)`
+        //     //client.user.setActivity(activ3);
+        // });
+    }, 10_000);
 });
 
 const words = ["NIGGER",
@@ -29,11 +55,11 @@ const words = ["NIGGER",
                 "DISCORD"];
 
 client.on("messageCreate", function(message){
-    console.log(`a message was created`);
-    console.log(message.author.username)
+    //console.log(`a message was created`);
+    //console.log(message)
     if (message.author.username == "Yeeti") {
         for (let embed of message.embeds) {
-            console.log(embed.description.toUpperCase());
+            //console.log(embed.description.toUpperCase());
             for (var i = 0; i < words.length; i++) {
                 let fuck = embed.description.toUpperCase();
                 if (fuck.includes(words[i])) {
@@ -52,7 +78,7 @@ client.on("messageCreate", function(message){
 
                     const exampleembed = new EmbedBuilder()
                         .setColor(0x0099FF)
-                        .setTitle('We have flagged a message on the server!')
+                        .setTitle(`We have flagged a message on the server!`)
                         .setDescription(`"${embed.description}"`)
     
                     client.channels.cache.get('1124893268602273882').send({
