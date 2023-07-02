fbfbc3695cab4889c8c9b5637936f3534235e8b1
RG2N <64289295+RG2N@users.noreply.github.com>
2023-07-02 00:17:37 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,8 +2,9 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get update
 sudo apt-get -y install nodejs
+sudo apt-get -y install npm
 sudo apt-get -y install unzip
 wget https://github.com/RG2N/bimsybot/archive/refs/heads/main.zip
-unzip bimsybot-main.zip
+unzip main.zip
 cd bimsybot-main
 npm install
