ab8aeade31cc097911c45daec4c91528bb5b3336
RG2N <64289295+RG2N@users.noreply.github.com>
2023-07-02 00:00:39 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,7 @@
-
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get update
+sudo apt-get install nodejs
+sudo apt-get install unzip
+wget https://github.com/RG2N/bimsybot/archive/refs/heads/main.zip
+unzip main.zip
